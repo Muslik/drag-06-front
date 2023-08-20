7f670c17884f2484dce762e78d06829020e7d50f
Dzhabrail <dzhabulya@gmail.com>
2023-08-20 20:00:49 +0300

    feat(generation): added api client generation from swagger

--- src/features/auth/loginGoogle/model.ts
+++ src/features/auth/loginGoogle/model.ts
@@ -2,7 +2,7 @@ import { createEvent, sample, attach } from 'effector';
 import { GoogleLoginResponse, GoogleLoginResponseOffline } from 'react-google-login';
 
 import { setSession } from '@drag/entities/session';
-import { api } from '@drag/shared/api';
+import { internalApi } from '@drag/shared/api';
 
 export const loginGoogleDone = createEvent<GoogleLoginResponse | GoogleLoginResponseOffline>();
 
@@ -12,16 +12,16 @@ const isGoogleLoginReponse = (
   return !response.code;
 };
 
-const loginGoogleFx = attach({ effect: api.loginGoogleApiFx });
+const loginGoogleFx = attach({ effect: internalApi.authLoginGoogleFx });
 
 sample({
   source: loginGoogleDone,
   fn: (response) => {
     if (isGoogleLoginReponse(response)) {
-      return { token: response.accessToken };
+      return { data: { token: response.accessToken } };
     }
 
-    return { token: response.code };
+    return { data: { token: response.code } };
   },
   target: loginGoogleFx,
 });
--- src/shared/api/internal/index.gen.ts
+++ src/shared/api/internal/index.gen.ts
@@ -1,5 +1,7 @@
 /* eslint-disable */
+
 /* tslint:disable */
+
 /*
  * ---------------------------------------------------------------
  * ## THIS FILE WAS GENERATED VIA SWAGGER-TYPESCRIPT-API        ##
@@ -8,6 +10,9 @@
  * ## SOURCE: https://github.com/acacode/swagger-typescript-api ##
  * ---------------------------------------------------------------
  */
+import { createEffect } from 'effector';
+
+import { requestFx } from '../request';
 
 export type LoginGoogleDto = {
   token: string;
@@ -39,7 +44,7 @@ export type ApiErrorResponse = {
    */
   message: string;
   /** Подробная информация об ошибке */
-  inner: object;
+  inner?: object;
 };
 
 export type JWTTokensDto = {
@@ -50,3 +55,273 @@ export type JWTTokensDto = {
 export type RefreshDto = {
   refreshToken: string;
 };
+
+export type EventEntity = {
+  id: string;
+  /** @format date-time */
+  createdDate: string;
+  /** @format date-time */
+  eventDate: string;
+  name: string;
+  eventStatus: 'registration' | 'started' | 'finished';
+};
+
+export type CreateEventDto = {
+  /** @format date-time */
+  eventDate: string;
+  name: string;
+};
+
+export type RequestValidationError = {
+  /**
+   * Поле у которого возникла ошибка
+   * @example "email"
+   */
+  property: string;
+  /**
+   * Описание каждой ошибки
+   * @example {"isNotEmpty":"Поле не может быть пустым"}
+   */
+  errors: Record<string, string>;
+  nested: object[];
+};
+
+export type ApiValidationErrorResponse = {
+  /**
+   * Тип ошибки
+   * @example "BAD_REQUEST"
+   */
+  type: string;
+  /**
+   * Код ошибки
+   * @example "VALIDATION_ERROR"
+   */
+  code: string;
+  /**
+   * Человеческое описание ошибки
+   * @example "Validation failed"
+   */
+  message: string;
+  /** Ошибки валидации */
+  inner: RequestValidationError[];
+};
+
+type AuthLoginGoogleParams = { data: LoginGoogleDto };
+
+export const authLoginGoogleFx = createEffect<
+  AuthLoginGoogleParams,
+  SessionUserDto,
+  ApiErrorResponse
+>({
+  async handler({ data }) {
+    const response = await requestFx({
+      path: '/auth/login/google',
+      method: 'post',
+      body: data,
+    });
+
+    if (response.status >= 400) {
+      throw response.body as ApiErrorResponse;
+    }
+
+    return response.body as SessionUserDto;
+  },
+});
+
+type AuthGetSessionUserParams = void;
+
+export const authGetSessionUserFx = createEffect<
+  AuthGetSessionUserParams,
+  SessionUserDto,
+  ApiErrorResponse
+>({
+  async handler() {
+    const response = await requestFx({
+      path: '/auth/session',
+      method: 'post',
+    });
+
+    if (response.status >= 400) {
+      throw response.body as ApiErrorResponse;
+    }
+
+    return response.body as SessionUserDto;
+  },
+});
+
+type AuthLogoutParams = void;
+
+export const authLogoutFx = createEffect<AuthLogoutParams, void, ApiErrorResponse>({
+  async handler() {
+    const response = await requestFx({
+      path: '/auth/logout',
+      method: 'post',
+    });
+
+    if (response.status >= 400) {
+      throw response.body as ApiErrorResponse;
+    }
+
+    return response.body as void;
+  },
+});
+
+type AuthLogoutAllParams = void;
+
+export const authLogoutAllFx = createEffect<AuthLogoutAllParams, void, ApiErrorResponse>({
+  async handler() {
+    const response = await requestFx({
+      path: '/auth/logout-all',
+      method: 'post',
+    });
+
+    if (response.status >= 400) {
+      throw response.body as ApiErrorResponse;
+    }
+
+    return response.body as void;
+  },
+});
+
+type UsersGetUsersParams = void;
+
+export const usersGetUsersFx = createEffect<UsersGetUsersParams, void, any>({
+  async handler() {
+    const response = await requestFx({
+      path: '/users',
+      method: 'get',
+    });
+
+    if (response.status >= 400) {
+      throw response.body as any;
+    }
+
+    return response.body as void;
+  },
+});
+
+type OauthLoginGoogleParams = { data: LoginGoogleDto };
+
+export const oauthLoginGoogleFx = createEffect<
+  OauthLoginGoogleParams,
+  JWTTokensDto,
+  ApiErrorResponse
+>({
+  async handler({ data }) {
+    const response = await requestFx({
+      path: '/oauth/login/google',
+      method: 'post',
+      body: data,
+    });
+
+    if (response.status >= 400) {
+      throw response.body as ApiErrorResponse;
+    }
+
+    return response.body as JWTTokensDto;
+  },
+});
+
+type OauthRefreshTokensParams = { data: RefreshDto };
+
+export const oauthRefreshTokensFx = createEffect<
+  OauthRefreshTokensParams,
+  JWTTokensDto,
+  ApiErrorResponse
+>({
+  async handler({ data }) {
+    const response = await requestFx({
+      path: '/oauth/refresh-tokens',
+      method: 'post',
+      body: data,
+    });
+
+    if (response.status >= 400) {
+      throw response.body as ApiErrorResponse;
+    }
+
+    return response.body as JWTTokensDto;
+  },
+});
+
+type EventsGetEventsParams = {
+  query?: {
+    take?: string;
+    skip?: string;
+    direction?: 'ASC' | 'DESC';
+    order?: 'eventDate' | 'name' | 'eventStatus';
+    search?: string;
+  };
+};
+
+export const eventsGetEventsFx = createEffect<
+  EventsGetEventsParams,
+  EventEntity[],
+  ApiErrorResponse
+>({
+  async handler({ query }) {
+    const response = await requestFx({
+      path: '/events',
+      method: 'get',
+      query,
+    });
+
+    if (response.status >= 400) {
+      throw response.body as ApiErrorResponse;
+    }
+
+    return response.body as EventEntity[];
+  },
+});
+
+type EventsCreateEventParams = { data: CreateEventDto };
+
+export const eventsCreateEventFx = createEffect<
+  EventsCreateEventParams,
+  EventEntity,
+  ApiValidationErrorResponse | ApiErrorResponse
+>({
+  async handler({ data }) {
+    const response = await requestFx({
+      path: '/events',
+      method: 'post',
+      body: data,
+    });
+
+    if (response.status >= 400) {
+      throw response.body as ApiValidationErrorResponse | ApiErrorResponse;
+    }
+
+    return response.body as EventEntity;
+  },
+});
+
+type EventsGetActiveEventsParams = {
+  query?: {
+    take?: string;
+    skip?: string;
+    direction?: 'ASC' | 'DESC';
+    order?: 'eventDate' | 'name' | 'eventStatus';
+    search?: string;
+  };
+};
+
+export const eventsGetActiveEventsFx = createEffect<
+  EventsGetActiveEventsParams,
+  EventEntity[],
+  ApiErrorResponse
+>({
+  async handler({ query }) {
+    const response = await requestFx({
+      path: '/events/active',
+      method: 'get',
+      query,
+    });
+
+    if (response.status >= 400) {
+      throw response.body as ApiErrorResponse;
+    }
+
+    return response.body as EventEntity[];
+  },
+});
