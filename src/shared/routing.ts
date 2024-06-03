af05a80d6caa6ac64edc5c64ae5d1bd630cf8959
Dzhabrail <dzhabulya@gmail.com>
2024-06-03 23:24:25 +0300

    feat(routing): updated routes

--- src/shared/routing.ts
+++ src/shared/routing.ts
@@ -11,18 +11,21 @@ import { appStarted } from './config/init';
 
 export const routes = {
   home: createRoute(),
-  tournament: createRoute<{ id: string | number }>(),
-  tournaments: createRoute(),
-  tournamentsRegister: createRoute<{ id: string | number }>(),
-  tournamentsCreate: createRoute(),
+  tournament: {
+    current: createRoute<{ tournamentId: string }>(),
+    all: createRoute(),
+    register: createRoute<{ tournamentId: string }>(),
+    create: createRoute(),
+  },
   notFound: createRoute(),
 };
 
 const routesMap: UnmappedRouteObject<any>[] = [
   { path: '/', route: routes.home },
-  { path: '/tournaments', route: routes.tournaments },
-  { path: '/tournaments/create', route: routes.tournamentsCreate },
-  { path: '/tournaments/:id/register', route: routes.tournamentsRegister },
+  { path: '/tournaments', route: routes.tournament.all },
+  { path: '/tournaments/create', route: routes.tournament.create },
+  { path: '/tournaments/:tournamentId', route: routes.tournament.current },
+  { path: '/tournaments/:tournamentId/register', route: routes.tournament.register },
   { path: '/404', route: routes.notFound },
 ];
 
